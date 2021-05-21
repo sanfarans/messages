fc02af22176a3892b2d5245d3df243a371def57b
sanfarans <jerzkra@gmail.com>
2021-05-21 23:13:38 +0200

    Add documentation

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Messages API',
+        default_version='v1',
+        description='Simple API to store messages',
+        license=openapi.License(name="BSD License")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0))
 ]
