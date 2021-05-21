9394e073015aec3c7420f80c9f9c41573141dc77
sanfarans <jerzkra@gmail.com>
2021-05-21 22:57:30 +0200

    Add authentication

--- api/tests.py
+++ api/tests.py
@@ -1,18 +1,19 @@
 import json
 
 from django.urls import reverse
-from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
 from .models import Message
 from .serializers import MessageSerializer
 
 
-class MessageViewTestCase(TestCase):
+class MessageViewTestCase(APITestCase):
 
     def setUp(self):
         obj = Message.objects.create(text='tests are very important')
+
 
     def test_message_view_get(self):
         response = self.client.get(reverse("view-message", kwargs={"message_id": 1}))
@@ -32,6 +33,8 @@ class MessageViewTestCase(TestCase):
         response = self.client.delete(reverse("view-message", kwargs={"message_id": 1}))
         self.assertEqual(len(Message.objects.all()), 0)
 
+class MessageCreateViewCase(APITestCase):
+    
     def test_create_view(self):
         response =  self.client.post(reverse("create-message"), {"text": "ok, tested"}, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
 
     'api',
 ]
@@ -125,3 +127,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+
+    ]
+}
